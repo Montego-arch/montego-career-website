578414b220702e9318b722ac0fe1ffa04852d527
Montego-arch <85162261+Montego-arch@users.noreply.github.com>
2023-08-10 09:44:53 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+JOBS = [
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location': 'Lagos, Nigeria',
+    'salary': '$8,000'
+  },
+    {
+    'id':2,
+    'title':'Data Scientist',
+    'location': 'Enugu State, Nigeria',
+    'salary': '$10,000'
+  },
+    {
+    'id':3,
+    'title':'DevOps Engineer',
+    'location': 'Florida, USA',
+    'salary': '$12,000'
+  },
+    {
+    'id':4,
+    'title':'Cloud Engineer',
+    'location': 'Remote',
+    
+  }
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
-
+  return render_template('home.html',
+                        jobs=JOBS,
+                        company_name='Montego')
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', debug=True)
